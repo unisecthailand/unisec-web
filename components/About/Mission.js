ac8f12f0f0c6a2ef49ace70f95ec712fbd054fa2
SecretKr <kraiwit.wit@gmail.com>
2025-07-29 12:39:48 +0700

    feat: update about page our mission

--- components/About/Mission.js
+++ components/About/Mission.js
@@ -7,7 +7,7 @@ const Mission = () => {
         <div className="flex flex-col justify-center w-full" data-aos="fade-up">
           <TextCard
             title="Our Mission"
-            description="Cultivate a supportive and motivating community for Thailand's space initiatives, with a primary focus on nurturing the potential of every Thai students."
+            description="Support and promote its members in the study and exploration of space technology affairs; to foster inspiration and motivation for the development of space technology; and to facilitate the exchange of knowledge and experiences among members and interested individuals. The Association also aims to organize workshops and educational activities to promote accessibility and understanding of space technology, and to coordinate and collaborate with legally recognized private organizations in advancing space technology. Furthermore, the Association supports freedom of thought and independent work in the field of space technology, encourages cooperation with other charitable organizations for public benefit."
           />
         </div>
         <div className="flex flex-col justify-center w-full" data-aos="fade-up">
