a4a557edd71908f764aedbdd254e7a2c68d3b1ac
unisecthailand <159617881+unisecthailand@users.noreply.github.com>
2026-07-18 17:58:14 +0700

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -5,12 +5,21 @@ import { useEffect } from "react";
 import AOS from "aos";
 import "aos/dist/aos.css";
 
+import Head from "next/head";
+
 function MyApp({ Component, pageProps, router }) {
   useEffect(() => {
     AOS.init({});
   }, []);
 
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <link rel="author" href="/humans.txt" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
